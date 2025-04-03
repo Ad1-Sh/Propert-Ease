d6126e736b8a6f7b6a6c17e506d00ead48c5fe30
Aditya <hi.aditya.in@gmail.com>
2025-04-03 18:01:28 +0530

    navigation added, header added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Profile from "./pages/Profile"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages SignUp"
-import ForgotPassword from "./pages/ForgotPassword"
-import Offers from "./pages/Offers"
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
      <Router>
+      <Header></Header>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-up" element={<SignUp/>} />
-        <Route path="/forgot-password" element={<ForgotPassword/>} />
-        <Route path="/offers" element={<Offers/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
       </Routes>
      </Router>
     </>
